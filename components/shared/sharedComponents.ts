0fc250eb30d2b50414694bff8812ea682680ac8d
Gustavo G Migacz <gustavomigacz@gmail.com>
2023-05-02 17:44:10 -0300

    create mainButton component

---
+++ components/shared/sharedComponents.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const GenericSection = styled.section`
+  max-width: var(--container-width);
+  padding: var(--container-padding);
+  margin: 0 auto;
+`;
+
+export const GenericSectionWrapper = styled.div`
+  width: 100%;
+`;
+
+export const MainButton = styled.button`
+  padding: 9px 38px;
+  border-radius: 4px;
+  color: #ffffffff;
+  font-family: var(--font-body);
+  font-weight: 600;
+  font-size: 18px;
+
+  background: linear-gradient(94.58deg, #8d5ac5 0%, #4967fd 100%);
+`;
